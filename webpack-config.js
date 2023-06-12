077236949b706030059d961dddbcfd435e491a14
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-06-12 02:40:49 +0000

    ADDED WEBPACK CONFIG.

---
+++ webpack-config.js
@@ -0,0 +1,15 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
